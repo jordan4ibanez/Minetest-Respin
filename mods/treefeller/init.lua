c076c49a2f7f97dbf4bcdb4c07e3cdb178f810f5
jordan4ibanez <jordan4ibanez@gmail.com>
2016-09-22 16:31:46 -0400

    update

--- mods/treefeller/init.lua
+++ mods/treefeller/init.lua
@@ -6,13 +6,12 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
 	local item = digger:get_wielded_item():to_string()
 	if string.match(item, "axe_") then
 		if minetest.get_item_group(oldnode.name, "tree") ~= 0 then
-			print("DO THE TREEFELLER LOOP")
 			treefeller_loop(pos,digger)
 		end
 	end
 end)
 
-treefeller_buffer = {}
+
 --have this recursively check for tree/leaves around it (1 node radius)
 treefeller_loop = function(pos,digger)
 
@@ -33,63 +32,75 @@ treefeller_loop = function(pos,digger)
 				local p_pos = area:index(pos.x+x,pos.y+y,pos.z+z)
 				local pos2 = {x=pos.x+x,y=pos.y+y,z=pos.z+z}
 				local name = minetest.get_name_from_content_id(data[p_pos])
-				--treefeller_buffer[digger:get_player_name()] = {pos.x+x,pos.y+y,pos.z+z}
-				if minetest.get_item_group(name, "tree") ~= 0 then
-					data[p_pos] = air
-					
-					minetest.after(0,function(pos2)
-						treefeller_loop(pos2)
-					end, pos2)
-					minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
-					--treecapitator.wear_tool(inv)
-					--if inv:room_for_item("main", name) == true then
-					--	inv:add_item("main", name)
-					--else
+				
+				
+				
+				if placed == nil or placed == "" then
+					if minetest.get_item_group(name, "tree") ~= 0 then
+						data[p_pos] = air
+						
+						minetest.after(0,function(pos2)
+							treefeller_loop(pos2)
+						end, pos2)
+						minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
+						--treecapitator.wear_tool(inv)
+						--if inv:room_for_item("main", name) == true then
+						--	inv:add_item("main", name)
+						--else
+						--	minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
+						--end
+						
+					--elseif minetest.get_item_group(name, "leaves") ~= 0 then
+					--	data[p_pos] = air
+					--	minetest.after(0,function(pos2)
+					--		treefeller_loop(pos2)
+					--	end, pos2)
 					--	minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
-					--end
-					
-				elseif minetest.get_item_group(name, "leaves") ~= 0 then
-					data[p_pos] = air
-					minetest.after(0,function(pos2)
-						treefeller_loop(pos2)
-					end, pos2)
-					minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
-					--treecapitator.wear_tool(inv)
-					--do random drops
-					--if math.random() > 0.9 then
-					--	local drop = minetest.registered_items[name]["drop"]
-					--	local tablelength = tablelength(drop.items)
-					--	local droplist = drop.items[math.random(1,tablelength)]
-					--	
-					--	local drop_item = droplist.items[1]
-					--		
-					--	if inv:room_for_item("main", drop_item) == true then
-					--		inv:add_item("main", drop_item)
-					--	else
-					--		minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, drop_item)
-					--	end
-					--end
+						--treecapitator.wear_tool(inv)
+						--do random drops
+						--if math.random() > 0.9 then
+						--	local drop = minetest.registered_items[name]["drop"]
+						--	local tablelength = tablelength(drop.items)
+						--	local droplist = drop.items[math.random(1,tablelength)]
+						--	
+						--	local drop_item = droplist.items[1]
+						--		
+						--	if inv:room_for_item("main", drop_item) == true then
+						--		inv:add_item("main", drop_item)
+						--	else
+						--		minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, drop_item)
+						--	end
+						--end
 
-				elseif minetest.get_item_group(name, "leafdecay") ~= 0 then
-					data[p_pos] = air
-					minetest.after(0,function(pos2)
-						treefeller_loop(pos2)
-					end, pos2)
-					minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
-					--treecapitator.wear_tool(inv)
-					--if inv:room_for_item("main", name) == true then
-					--	inv:add_item("main", name)
-					--else
+					--elseif minetest.get_item_group(name, "leafdecay") ~= 0 then
+					--	data[p_pos] = air
+					--	minetest.after(0,function(pos2)
+					--		treefeller_loop(pos2)
+					--	end, pos2)
 					--	minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
-					--end	
+						--treecapitator.wear_tool(inv)
+						--if inv:room_for_item("main", name) == true then
+						--	inv:add_item("main", name)
+						--else
+						--	minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, name)
+						--end	
+					end
 				end
 			end
 		end
 	end
 	
-	
+	vm:update_liquids()
 	vm:set_data(data)
 	vm:calc_lighting()
 	vm:write_to_map()
 	vm:update_map()
 end
+
+minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
+    if oldnode ~= nil then
+        print("Node " .. oldnode.name .. " at " ..  minetest.pos_to_string(pos) .. " replaced with " .. newnode.name .. "  by " .. placer:get_player_name())
+    else
+        print("Node " .. newnode.name .. " at " ..  minetest.pos_to_string(pos) .. " placed by " .. placer:get_player_name())
+    end
+end)
