a546a801999121e34f6bf4b8212adb4a67afefe9
jordan4ibanez <jordan4ibanez@gmail.com>
2016-09-23 19:42:38 -0400

    Tweaks and updates

--- mods/rs/init.lua
+++ mods/rs/init.lua
@@ -2,11 +2,11 @@ minetest.register_entity("rs:r", { --basic minecart
 
 	physical     = true,
 	collisionbox = {-0.45, -0.45, -0.45, 0.45, 0.45, 0.45},
-	visual       = mesh,
-	mesh         = "basic_minecart.x",
-	textures     = "minecart.png",
+	visual       = "mesh",
+	mesh         = "minecart.x",
+	textures     = {"minecart.png"},
 	visual_size = {x=1, y=1},
-	stepheight = 1,
+	stepheight = 0.7,
 	automatic_face_movement_dir = 0,
 	timer = 0,
 	collide_with_objects = false,
@@ -18,20 +18,24 @@ minetest.register_entity("rs:r", { --basic minecart
 	dir_y = 0,
 	dir_z = 0,
 	
+	speed = 0,
+	
 	--punch function
 	on_punch = function(self)
-
 	end,
 
 	--right click function
 	on_rightclick = function(self, clicker)
-	
+		self.dir_x = 1
+		--self.obj
+		self.speedup = true
+		clicker:set_attach(self.object, "", {x=0,y=0,z=0}, {x=0,y=0,z=0})
 	end,
 
 	--when the entity is created in world
 	on_activate = function(self, staticdata, dtime_s)
-		self.object:set_armor_groups({immortal=1})
-
+		--self.object:set_armor_groups({immortal=1})
+		self.object:set_animation({x=45,y=45},0, 0)
 		self.object:setacceleration({x=0,y=-10,z=0})		
 	end,
 	get_staticdata = function(self)
@@ -47,47 +51,48 @@ minetest.register_entity("rs:r", { --basic minecart
 	--what the mob does in the world
 	on_step = function(self, dtime)
 		self.timer = self.timer + dtime
+		if self.speedup == true then
+			self.speed = self.speed + dtime
+		end
 		
 		local pos = self.object:getpos()
 		local vel = self.object:getvelocity()
-		
-		local goal_pos = {x=pos.x+self.dir_x,y=pos.y+self.dir_y,z=pos.z+self.dir_z}
-		
-		if self.dir_x == 0 then
-			if string.match(minetest.get_node({x=pos.x+1,y=pos.y,z=pos.z}).name, "rail") then
-				self.dir_x = 1
-			elseif string.match(minetest.get_node({x=pos.x-1,y=pos.y,z=pos.z}).name, "rail")   then
-				self.dir_x = -1
-			end
+		if vel.y < 0 then
+			self.object:set_animation({x=0,y=0},0, 0)
+		elseif vel.y > 0 then
+			self.object:set_animation({x=90,y=90},0, 0)
+		else
+			self.object:set_animation({x=45,y=45},0, 0)
 		end
-		if self.dir_z == 0 then
-			if string.match(minetest.get_node({x=pos.x,y=pos.y,z=pos.z+1}).name, "rail")  then
+		
+		--self.speed = 5
+		if self.dir_x ~= 0 and vel.x == 0 then
+			if minetest.get_node({x=pos.x,y=pos.y,z=pos.z+1}).name == "air" then
 				self.dir_z = 1
-			elseif  string.match(minetest.get_node({x=pos.x,y=pos.y,z=pos.z-1}).name, "rail")  then
+				self.dir_x = 0
+			elseif minetest.get_node({x=pos.x,y=pos.y,z=pos.z-1}).name == "air" then
 				self.dir_z = -1
+				self.dir_x = 0
+			end
+		elseif self.dir_z ~= 0 and vel.z == 0 then
+			if minetest.get_node({x=pos.x+1,y=pos.y,z=pos.z}).name == "air" then
+				self.dir_x = 1
+				self.dir_z = 0
+			elseif minetest.get_node({x=pos.x-1,y=pos.y,z=pos.z}).name == "air" then
+				self.dir_x = -1
+				self.dir_z = 0
 			end
 		end
 		
+		self.goal = {x=self.dir_x*self.speed,y=-10,z=self.dir_z*self.speed}
 		
+		self.object:setvelocity(self.goal)
 
-		
-		--print(minetest.get_item_group(minetest.get_node({x=pos.x+1,y=pos.y,z=pos.z}).name, "rail"))
-							
-		local pos1 = pos
-		--pos1.y = pos1.y+item_drop_settings.player_collect_height
-		local object = minetest.get_player_by_name("singleplayer")
-		local pos2 = goal_pos
-		local vec = {x=pos1.x-pos2.x, y=pos1.y-pos2.y, z=pos1.z-pos2.z}
-
-		self.vel_goal_x = (vec.x*-4) -- do speed instead
-		self.vel_goal_y = (vec.y*-4)
-		self.vel_goal_z = (vec.z*-4)
-		for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 0.6)) do
-			if object:is_player() then
-				self.vel_goal_x = 0 -- this is a lazy workaround
-				self.vel_goal_y = 0
-				self.vel_goal_z = 0
-			end
-		end
 	end,
 	})
+
+minetest.override_item("default:stick", {
+	on_place = function(itemstack, placer, pointed_thing)
+		minetest.add_entity(pointed_thing.above, "rs:r")
+	end,
+})
