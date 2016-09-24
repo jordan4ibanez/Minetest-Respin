f401900f5028f072349de434b59c9e8f17f98440
jordan4ibanez <jordan4ibanez@gmail.com>
2016-09-24 11:25:42 -0400

    Add in upgrades

--- mods/upgrade/init.lua
+++ mods/upgrade/init.lua
@@ -5,6 +5,12 @@ minetest.register_craftitem("upgrade:rsamble", {
 
 --make upgrades wear like tools
 
+-- auto places slabs/stairs as you walk
+minetest.register_craftitem("upgrade:auto_stair", {
+	description = "Auto Stair Upgrade",
+	inventory_image = "auto_stair.png",
+})
+
 --makes a radius of shovel/pick digs
 minetest.register_craftitem("upgrade:borer_1", {
 	description = "Borer Upgrade 1",
@@ -38,7 +44,7 @@ minetest.register_craftitem("upgrade:hp_boost", {
 })
 
 --place a ladder when digging straight down
-minetest.register_craftitem("upgrade:", {
+minetest.register_craftitem("upgrade:auto_ladder", {
 	description = "",
 	inventory_image = ".png",
 })
@@ -73,7 +79,7 @@ minetest.register_craftitem("upgrade:auto_torch", {
 	inventory_image = ".png",
 })
 
-borer_function = function(pos,digger,radius,item)
+borer_function = function(pos,digger,radius,item,autoladder)
 
 	local min = {x=pos.x-radius,y=pos.y-radius,z=pos.z-radius}
 	local max = {x=pos.x+radius,y=pos.y+radius,z=pos.z+radius}
@@ -88,8 +94,9 @@ borer_function = function(pos,digger,radius,item)
 	elseif  string.match(item, "shovel_") then
 		typer = "crumbly"
 	end
-	
+	print(autoladder)
 	local air = minetest.get_content_id("air")
+	local ladder = minetest.get_content_id("default:ladder")
 	for x = -radius,radius  do
 		for z = -radius,radius  do
 			for y = -radius,radius  do
@@ -101,8 +108,12 @@ borer_function = function(pos,digger,radius,item)
 				
 				if placed == nil or placed == "" then
 					if minetest.get_item_group(name, typer) > 0 then 
-					--if string.match(name, "stone_with_") then
-						data[p_pos] = air
+						--autoladder
+						if autoladder == true and x == 0 and z == 0 then
+							data[p_pos] = ladder
+						else
+							data[p_pos] = air
+						end
 						local item = minetest.get_node_drops(name)[1]
 						minetest.add_item({x=pos.x+x,y=pos.y+y,z=pos.z+z}, item)	
 					end
@@ -123,13 +134,84 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
 	local item = digger:get_wielded_item():to_string()
 	--find if item is pick/axe/shovel
 	local inv = digger:get_inventory()
+	local autoladder = inv:contains_item("main", "upgrade:auto_ladder")
 	
+	--search for the borer upgrade, which does almost every upgrade in the inventory a bunch of times, else do them once
 	if inv:contains_item("main", "upgrade:borer_1") then
-		borer_function(pos,digger,1,item)
+		borer_function(pos,digger,1,item,autoladder)
 	elseif inv:contains_item("main", "upgrade:borer_2") then
-		borer_function(pos,digger,2,item)
+		borer_function(pos,digger,2,item,autoladder)
 	elseif inv:contains_item("main", "upgrade:borer_3") then
-		borer_function(pos,digger,3,item)
+		borer_function(pos,digger,3,item,autoladder)
+	else
+	
+		print("rewrite all the upgrades for single use")
 	end
 	
 end)
+
+
+-- do all buckets at once
+--set_wielded_item(item)
+local function check_protection(pos, name, text)
+	if minetest.is_protected(pos, name) then
+		minetest.log("action", (name ~= "" and name or "A mod")
+			.. " tried to " .. text
+			.. " at protected position "
+			.. minetest.pos_to_string(pos)
+			.. " with a bucket")
+		minetest.record_protection_violation(pos, name)
+		return true
+	end
+	return false
+end
+
+minetest.override_item("bucket:bucket_empty", {
+	on_use = function(itemstack, user, pointed_thing)
+		-- Must be pointing to node
+		if pointed_thing.type ~= "node" then
+			return
+		end
+		-- Check if pointing to a liquid source
+		local node = minetest.get_node(pointed_thing.under)
+		local liquiddef = bucket.liquids[node.name]
+		local item_count = user:get_wielded_item():get_count()
+
+		if liquiddef ~= nil
+		and liquiddef.itemname ~= nil
+		and node.name == liquiddef.source then
+			if check_protection(pointed_thing.under,
+					user:get_player_name(),
+					"take ".. node.name) then
+				return
+			end
+
+			-- default set to return filled bucket
+			local giving_back = liquiddef.itemname
+
+			-- check if holding more than 1 empty bucket
+			if item_count > 1 then
+				for i = 1,item_count-1 do
+					print("Test")
+					-- if space in inventory add filled bucked, otherwise drop as item
+					local inv = user:get_inventory()
+					if inv:room_for_item("main", {name=liquiddef.itemname}) then
+						inv:add_item("main", liquiddef.itemname)
+					else
+						local pos = user:getpos()
+						pos.y = math.floor(pos.y + 0.5)
+						minetest.add_item(pos, liquiddef.itemname)
+					end
+					-- set to return empty buckets minus 1
+					--giving_back = "bucket:bucket_empty "..tostring(item_count-item_count)
+				end
+				user:set_wielded_item(liquiddef.itemname)
+
+			end
+
+			minetest.add_node(pointed_thing.under, {name="air"})
+
+			return ItemStack(giving_back)
+		end
+	end,
+})
