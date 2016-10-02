05926493351b4380195a855f1090e44bd621cb62
jordan4ibanez <jordan4ibanez@gmail.com>
2016-10-02 19:36:44 -0400

    Add in colored fences

--- mods/colored_wood/init.lua
+++ mods/colored_wood/init.lua
@@ -3,6 +3,7 @@ local wood_table = {"default:wood","default:junglewood","default:pine_wood","def
 local colors = {{"white","#FFFFFF"},{"grey","#808080"},{"dark_grey","#404040"},{"black","#000000"},{"cyan","#00FFFF"},{"green","#008000"},{"dark_green","#004000"},
 {"yellow","#FFFF00"},{"orange","#FFA500"},{"brown","#614126"},{"pink","#FFC0CB"},{"magenta","#FF00FF"},{"violet","#EE82EE"},{"blue", "#0000ff"},{"red","#FF0000"},}
 
+--wood
 for key,value in pairs(wood_table) do
 	local def = minetest.registered_items[value]
 	for number,colortable in pairs(colors) do
@@ -22,6 +23,36 @@ for key,value in pairs(wood_table) do
 			recipe = {value,  "dye:"..colortable[1]},
 		})
 		--return wood to normal
+		minetest.register_craft( {
+			type = "shapeless",
+			output = value,
+			recipe = {"colored_wood:"..string.gsub(value, "default:", "").."_"..colors[number][1]},
+		})
+	end
+end
+
+--fences
+for key,value in pairs(wood_table) do
+	local value = "default:fence_"..string.gsub(value, "default:", "")
+	local def = minetest.registered_items[value]
+	for number,colortable in pairs(colors) do
+		default.register_fence("colored_wood:"..string.gsub(value, "default:", "").."_"..colors[number][1], {
+			description = def.description.." "..colors[number][1]:gsub("^%l", string.upper),
+			texture = def.tiles[1].."^[colorize:"..colortable[2]..":170",
+			inventory_image = def.inventory_image.."^[colorize:"..colortable[2]..":170",
+			wield_image = def.wield_image.."^[colorize:"..colortable[2]..":170",
+			material = string.gsub(value, "fence_", ""),
+			groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+			sounds = default.node_sound_wood_defaults()
+		})
+		
+		--dye fence
+		minetest.register_craft( {
+			type = "shapeless",
+			output = "colored_wood:"..string.gsub(value, "default:", "").."_"..colors[number][1],
+			recipe = {value,  "dye:"..colortable[1]},
+		})
+		--return fence to normal
 		minetest.register_craft( {
 			type = "shapeless",
 			output = value,
