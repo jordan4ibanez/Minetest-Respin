19ac9caa23f4cb19d2406cc3039227714131bf93
jordan4ibanez <jordan4ibanez@gmail.com>
2016-10-02 15:16:09 -0400

    Add in world_assistant mod

--- mods/world_assistant/init.lua
+++ mods/world_assistant/init.lua
@@ -1,95 +1,78 @@
 world_assistant = {}
 
-
-
---[[
-minetest.register_globalstep(function(dtime)
-	for _,player in ipairs(minetest.get_connected_players()) do
-		local pos = player:getpos()
-		local lookdir = player:get_look_dir()
-		--this isn't even close to perfect
-		for i = 1,2 do
-			local node = minetest.get_node({x=pos.x+(lookdir.x*i),y=pos.y+1.75+(lookdir.y*i),z=pos.z+(lookdir.z*i)}).name
-			if node ~= "air" and minetest.registered_items[node].pointable == true then
-				if node_help[player:get_player_name()] == nil then
-					node_help[player:get_player_name()] = {}
-					node_help[player:get_player_name()]["id"] = 0
-					node_help[player:get_player_name()]["node"] = "air"
-				end
-				if node_help[player:get_player_name()]["id"] ~= nil then
-					if node_help[player:get_player_name()]["node"] ~= node then
-
-						player:hud_remove(node_help[player:get_player_name()]["id"])
-						
-						local text = ""
-						--print(dump(minetest.registered_items[node]))
-						text = text.."Mod:  "..minetest.registered_items[node].mod_origin.."\n"
-						text = text.."Node: "..minetest.registered_items[node].description.."\n"
-						if minetest.registered_items[node].groups.crumbly > 0 then
-							text = text.."Tool: Shovel\n"
-						elseif  minetest.registered_items[node].groups.cracky > 0 then
-							text = text.."Tool: Pick\n"
-						elseif  minetest.registered_items[node].groups.choppy > 0 then
-							text = text.."Tool: Axe\n"
-						end
-						local id = player:hud_add({
-							hud_elem_type = "text",
-							position = {x = 0.5, y = 0.1},
-							scale = {
-								x = -100,
-								y = -100
-							},
-						text = text,
-						})
-						node_help[player:get_player_name()]["id"] = id
-						node_help[player:get_player_name()]["node"] = node
-						return
-					end
-				end
-			end
-		end
-		
-		if node_help[player:get_player_name()] ~= nil then
-			if node_help[player:get_player_name()]["id"] ~= nil then
-				player:hud_remove(node_help[player:get_player_name()]["id"])
-				node_help[player:get_player_name()]["node"] = ""
-				node_help[player:get_player_name()]["id"] = nil
-			end
-		end
-		
-	end
-end)
-]]--		
-
---[[
---start registering things
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-		--try to understand the node (they seem to talk chinese)
-		local node = minetest.get_node_or_nil(pointed_thing.under)
-		local name = node.name
-		--send what we know from the lolling node
-		local id1 = puncher:hud_add({
+	local mod_name  = minetest.registered_items[node.name].mod_origin
+	local node_name = minetest.registered_items[node.name].description
+
+	local tool = ""
+	if minetest.registered_items[node.name].groups ~= nil then
+		if minetest.registered_items[node.name].groups.crumbly then
+			tool = "Shovel"
+		elseif  minetest.registered_items[node.name].groups.cracky then
+			tool = "Pick"
+		elseif  minetest.registered_items[node.name].groups.choppy  then
+			tool = "Axe"
+		else
+			tool = "Hand"
+		end
+	end
+	
+	local text = node_name.."\nMod: "..mod_name.."\nTool: "..tool
+	
+	--create blank hud table
+	if world_assistant[puncher:get_player_name()] == nil then
+		world_assistant[puncher:get_player_name()] = {}
+	end
+
+	--remove all old huds
+	if world_assistant[puncher:get_player_name()]["id1"] ~= nil then
+		puncher:hud_remove(world_assistant[puncher:get_player_name()]["id1"])
+	end
+	if world_assistant[puncher:get_player_name()]["background"] ~= nil then
+		puncher:hud_remove(world_assistant[puncher:get_player_name()]["background"])
+	end
+	
+	--reset the hud remove timer
+	world_assistant[puncher:get_player_name()]["time"] = 0
+	
+	local background = puncher:hud_add({
 			hud_elem_type = "image",
-			position = {x = 0.5, y = 0.02},
+			position = {x = 0.5, y = 0.06},
 			scale = {
 				x = -26,
 				y = -12
 			},
-		text = "NodeExplorer_HUD.png",
+		text = "world_assistant_background.png",
 		})
-		local id2 = puncher:hud_add({
-			hud_elem_type = "text",
-			position = {x = 0.5, y = 0.03},
-			scale = {
-				x = -100,
-				y = -100
-			},
-		text = "  You have touched: \n''" .. name .. "''."
-		})
-		--then remove it
-	minetest.after(1.1, function(name)
-	puncher:hud_remove(id1)
-	puncher:hud_remove(id2)
-	end)	
+	local id = puncher:hud_add({
+		hud_elem_type = "text",
+		position = {x = 0.5, y = 0.06},
+		number = 0xFFFFFF,
+		scale = {
+			x = -100,
+			y = -100
+		},
+	text = text,
+	})
+	
+	world_assistant[puncher:get_player_name()]["id1"] = id
+	world_assistant[puncher:get_player_name()]["background"] = background
+
 end)
-]]--
+minetest.register_globalstep(function(dtime)
+	for _,player in ipairs(minetest.get_connected_players()) do
+		if world_assistant[player:get_player_name()] ~= nil then
+			world_assistant[player:get_player_name()]["time"] = world_assistant[player:get_player_name()]["time"] + dtime
+			if world_assistant[player:get_player_name()]["time"] > 2 then
+				--remove all old huds
+				if world_assistant[player:get_player_name()]["id1"] ~= nil then
+					player:hud_remove(world_assistant[player:get_player_name()]["id1"])
+				end
+				if world_assistant[player:get_player_name()]["background"] ~= nil then
+					player:hud_remove(world_assistant[player:get_player_name()]["background"])
+				end			
+			end
+		end
+	end
+end)
+
