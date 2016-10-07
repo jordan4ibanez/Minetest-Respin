ed751dcbc5399043fbb1a1fe7d2e2655951571f9
jordan4ibanez <jordan4ibanez@gmail.com>
2016-10-06 21:55:29 -0400

    Lower hurt volume

--- mods/player_mods/init.lua
+++ mods/player_mods/init.lua
@@ -6,13 +6,13 @@ minetest.register_on_player_hpchange(function(player, hp_change)
 		minetest.sound_play("player_hurt", {
 			pos = pos,
 			max_hear_distance = 10,
-			gain = 1.0,
+			gain = 0.5,
 		})
 	elseif health <= 0 and hp_change <= 0 and player_hurt_table[player:get_player_name()] == nil then
 		minetest.sound_play("player_death", {
 			pos = pos,
 			max_hear_distance = 10,
-			gain = 1.0,
+			gain = 0.5,
 		})
 		--prevent spamming of death sound
 		player_hurt_table[player:get_player_name()] = true
