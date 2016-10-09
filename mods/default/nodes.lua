d25d69fa4fd877cf11c21261891fdac1671fc2f9
jordan4ibanez <jordan4ibanez@gmail.com>
2016-10-08 21:19:59 -0400

    Make snow attached instead of falling to improve performance and give better feel

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -430,7 +430,7 @@ minetest.register_node("default:snow", {
 			{-0.5, -0.5, -0.5, 0.5, -0.25, 0.5},
 		},
 	},
-	groups = {crumbly = 3, falling_node = 1, puts_out_fire = 1},
+	groups = {crumbly = 3, attached_node = 1},
 	sounds = default.node_sound_dirt_defaults({
 		footstep = {name = "default_snow_footstep", gain = 0.15},
 		dug = {name = "default_snow_footstep", gain = 0.2},
