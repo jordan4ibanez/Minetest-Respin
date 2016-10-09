ad82d8640c504b43dcbfc23048a1978d58218e66
jordan4ibanez <jordan4ibanez@gmail.com>
2016-10-08 21:16:18 -0400

    Neaten up colored wood code and fix naming

--- mods/colored_wood/init.lua
+++ mods/colored_wood/init.lua
@@ -1,14 +1,35 @@
-local wood_table = {"default:wood","default:junglewood","default:pine_wood","default:acacia_wood","default:aspen_wood"}
+local wood_table = {
+"default:wood",
+"default:junglewood",
+"default:pine_wood",
+"default:acacia_wood",
+"default:aspen_wood"
+}
 
-local colors = {{"white","#FFFFFF"},{"grey","#808080"},{"dark_grey","#404040"},{"black","#000000"},{"cyan","#00FFFF"},{"green","#008000"},{"dark_green","#004000"},
-{"yellow","#FFFF00"},{"orange","#FFA500"},{"brown","#614126"},{"pink","#FFC0CB"},{"magenta","#FF00FF"},{"violet","#EE82EE"},{"blue", "#0000ff"},{"red","#FF0000"},}
+local colors = {
+{"white",     "#FFFFFF", "White"},
+{"grey",      "#808080", "Grey"},
+{"dark_grey", "#404040", "Dark Grey"},
+{"black",     "#000000", "Black"},
+{"cyan",      "#00FFFF", "Cyan"},
+{"green",     "#008000", "Green"},
+{"dark_green","#004000", "Dark Green"},
+{"yellow",    "#FFFF00", "Yellow"},
+{"orange",    "#FFA500", "Orange"},
+{"brown",     "#614126", "Brown"},
+{"pink",      "#FFC0CB", "Pink"},
+{"magenta",   "#FF00FF", "Magenta"},
+{"violet",    "#EE82EE", "Violet"},
+{"blue",      "#0000ff", "Blue"},
+{"red",       "#FF0000", "Red"}
+}
 
 --wood
 for key,value in pairs(wood_table) do
 	local def = minetest.registered_items[value]
 	for number,colortable in pairs(colors) do
 		minetest.register_node("colored_wood:"..string.gsub(value, "default:", "").."_"..colors[number][1], {
-			description = colors[number][1]:gsub("^%l", string.upper).." "..def.description,
+			description = colors[number][3].." "..def.description,
 			paramtype2 = "facedir",
 			place_param2 = 0,
 			tiles = {def.tiles[1].."^[colorize:"..colortable[2]..":170"},
@@ -37,7 +58,7 @@ for key,value in pairs(wood_table) do
 	local def = minetest.registered_items[value]
 	for number,colortable in pairs(colors) do
 		default.register_fence("colored_wood:"..string.gsub(value, "default:", "").."_"..colors[number][1], {
-			description = colors[number][1]:gsub("^%l", string.upper).." "..def.description,
+			description = colors[number][3].." "..def.description,
 			texture = def.tiles[1].."^[colorize:"..colortable[2]..":170",
 			inventory_image = def.inventory_image.."^[colorize:"..colortable[2]..":170",
 			wield_image = def.wield_image.."^[colorize:"..colortable[2]..":170",
