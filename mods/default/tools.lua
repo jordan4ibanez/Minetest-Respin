43522b7a06485160832fb13cf39e231d8db2b10e
jordan4ibanez <jordan4ibanez@gmail.com>
2016-10-27 13:54:08 -0400

    Make hand bigger

--- mods/default/tools.lua
+++ mods/default/tools.lua
@@ -4,7 +4,7 @@
 minetest.register_item(":", {
 	type = "none",
 	wield_image = "wieldhand.png",
-	wield_scale = {x=1,y=1,z=2.5},
+	wield_scale = {x=1.5,y=2.5,z=5},
 	tool_capabilities = {
 		full_punch_interval = 0.9,
 		max_drop_level = 0,
